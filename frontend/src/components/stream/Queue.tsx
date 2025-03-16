3487f0fb27f9ff9c68090e736ba53711041682dd
Karan <karansharma192004@gmail.com>
2025-03-16 22:52:01 +0530

    Updated queue

---
+++ frontend/src/components/stream/Queue.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import { Button } from "../ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "../ui/input";
+
+export default function Queue() {
+
+  const [videos, setvideos] = useState([])
+
+  return (
+    <div className="w-72">
+     
+        <div className="flex flex-col gap-4">
+          <span className="text-xl font-semibold">Videos in Queue</span>
+          <div className="w-full h-20 border border-white rounded-md p-1 flex text-white gap-4 items-center">
+          <img src="https://img.youtube.com/vi/QvXVNyqOMv4/0.jpg" alt="image" className="w-30 h-full" />
+          <p>Added by karan</p>
+          </div>
+          <div className="w-full h-20 border border-white rounded-md p-1 flex text-white gap-4 items-center">
+          <img src="https://img.youtube.com/vi/QvXVNyqOMv4/0.jpg" alt="image" className="w-30 h-full" />
+          <p>Added by karan</p>
+          </div>
+          <div className="w-full h-20 border border-white rounded-md p-1 flex text-white gap-4 items-center">
+          <img src="https://img.youtube.com/vi/QvXVNyqOMv4/0.jpg" alt="image" className="w-30 h-full" />
+          <p>Added by karan</p>
+          </div>
+          <div className="w-full h-20 border border-white rounded-md p-1 flex text-white gap-4 items-center">
+          <img src="https://img.youtube.com/vi/QvXVNyqOMv4/0.jpg" alt="image" className="w-30 h-full" />
+          <p>Added by karan</p>
+          </div>
+          <div className="w-full h-20 border border-white rounded-md p-1 flex text-white gap-4 items-center">
+          <img src="https://img.youtube.com/vi/QvXVNyqOMv4/0.jpg" alt="image" className="w-30 h-full" />
+          <p>Added by karan</p>
+          </div>
+          
+       <Dialog>
+       <DialogTrigger className="bg-white text-black font-semibold rounded-md px-3 py-2 w-full">Add video</DialogTrigger>
+       <DialogContent className="w-96">
+         <DialogHeader>
+           <DialogTitle>Add your favorites in Queue</DialogTitle>
+           <DialogDescription>
+             This action cannot be undone.
+           </DialogDescription>
+           <Input placeholder="Enter youtube video url"/>
+           <Button>Add</Button>
+         </DialogHeader>
+       </DialogContent>
+     </Dialog>
+    </div>
+      </div>
+  );
+}
--- frontend/src/pages/Stream.tsx
+++ frontend/src/pages/Stream.tsx
@@ -1,4 +1,5 @@
 import Chatbox from "@/components/stream/Chatbox";
+import Queue from "@/components/stream/Queue";
 import Video from "@/components/stream/Video";
 import { useSocket } from "@/hooks/useSocket";
 
@@ -6,12 +7,13 @@ export default function Stream() {
     const {socket, messages} = useSocket();
     
   return (
-    <div className="w-screen h-screen bg-black text-white">
+    <div className="w-screen h-screen bg-black text-white relative">
       <div className="flex w-full h-full justify-between">
-        <div className="w-[75%] flex justify-center">
+        <div className="w-[75%] flex">
+            <Queue/>
             <Video/>
         </div>
-        <div className="w-[25%] h-full border-red-100 border">
+        <div className="w-[25%] h-full border-red-100 border-l-[1px] fixed top-0 right-0">
           <Chatbox socket={socket} messages={messages} />
         </div>
       </div>

c3c3b7f246bd1353ece72b2c8fa089863c890a7b
Karan <karansharma192004@gmail.com>
2025-03-18 23:21:04 +0530

    Fixed fetch videos

--- frontend/src/components/stream/Queue.tsx
+++ frontend/src/components/stream/Queue.tsx
@@ -14,18 +14,20 @@ import { BACKEND_URL } from "@/lib/config";
 import { data } from "react-router-dom";
 
 interface videoInterface{
+ videos:{
   id: number,
   url: string,
   upvote: number,
   userId: string
+ }
 }
 export default function Queue() {
-  const [videos, setVideos] = useState([])
+  const [videos, setVideos] = useState<any>()
   useEffect(()=>{
     const fetchVideos = async () => {
         const res = await axios.get<videoInterface>(`${BACKEND_URL}/videos`);
         console.log(res.data.videos);
-        setVideos(prev => [...prev, res.data.videos])
+        setVideos(res.data.videos)
     };
 
     fetchVideos(); 
@@ -59,22 +61,7 @@ export default function Queue() {
           <img src="https://img.youtube.com/vi/QvXVNyqOMv4/0.jpg" alt="image" className="w-30 h-full" />
           <p>Added by karan</p>
           </div>
-          <div className="w-full h-20 border border-white rounded-md p-1 flex text-white gap-4 items-center">
-          <img src="https://img.youtube.com/vi/QvXVNyqOMv4/0.jpg" alt="image" className="w-30 h-full" />
-          <p>Added by karan</p>
-          </div>
-          <div className="w-full h-20 border border-white rounded-md p-1 flex text-white gap-4 items-center">
-          <img src="https://img.youtube.com/vi/QvXVNyqOMv4/0.jpg" alt="image" className="w-30 h-full" />
-          <p>Added by karan</p>
-          </div>
-          <div className="w-full h-20 border border-white rounded-md p-1 flex text-white gap-4 items-center">
-          <img src="https://img.youtube.com/vi/QvXVNyqOMv4/0.jpg" alt="image" className="w-30 h-full" />
-          <p>Added by karan</p>
-          </div>
-          <div className="w-full h-20 border border-white rounded-md p-1 flex text-white gap-4 items-center">
-          <img src="https://img.youtube.com/vi/QvXVNyqOMv4/0.jpg" alt="image" className="w-30 h-full" />
-          <p>Added by karan</p>
-          </div>
+        
           
        <Dialog>
        <DialogTrigger className="bg-white text-black font-semibold rounded-md px-3 py-2 w-full">Add video</DialogTrigger>
